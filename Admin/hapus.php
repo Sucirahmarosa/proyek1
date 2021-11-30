76fa07142c9b4482f58c33bfcc1a73a10e50bd68
Ramadhaniyanto19 <mramadhaniyanto@gmail.com>
2021-11-30 16:36:54 +0700

    Test comit

--- Admin/hapus.php
+++ Admin/hapus.php
@@ -7,11 +7,11 @@ $id = $_GET["id"];
 var_dump($id);
 
 if( hapus($id) > 0){
-      echo "<script> alert('Data berhasil dihapus!');
+      echo "<script> alert('Data berhasil dihapus');
         document.location.href = 'produk.php';
       </script>";
  }else{
-       echo "<script> alert('Data gagal dihapus!');
+       echo "<script> alert('Data gagal dihapus');
         document.location.href = 'produk.php';
       </script>";
  }
