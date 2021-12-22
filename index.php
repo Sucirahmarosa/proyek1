90e6d500011376864771d7313ca4abb84bef38a3
Ochasucirr <88840237+Sucirahmarosa@users.noreply.github.com>
2021-12-22 14:36:49 +0700

    Update index.php
    ganti judul jadi proyek1

--- index.php
+++ index.php
@@ -38,7 +38,7 @@ require 'function.php';
     <!-- Navigation-->
     <nav class="navbar navbar-expand-lg navbar-dark bg-dark sticky-top">
         <div class="container">
-            <a class="navbar-brand fs-3 fw-bold" href="#">TokoKita</a>
+            <a class="navbar-brand fs-3 fw-bold" href="#">Proyek1</a>
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                 <span class="navbar-toggler-icon"></span>
             </button>
